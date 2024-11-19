79cad0e9687f6ec33a35232ff5f4f46df452c39f
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-11-19 23:35:48 +0530

    vite changes

--- insta_clone/vite.config.js
+++ insta_clone/vite.config.js
@@ -1,27 +1,14 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: undefined, // Optional: single file builds
-//       },
-//     },
-//   },
-//   base: '/Insta-clone/', // Ensure this matches your repository name
-// });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        historyApiFallback: true, // Serves index.html for unknown routes
-    },
     build: {
-        outDir: 'dist', // Ensure your build output folder is correct
+        rollupOptions: {
+            output: {
+                manualChunks: undefined, // Optional: single file builds
+            },
+        },
     },
+    base: "/Insta-clone/", // Ensure this matches your repository name
 });
-
